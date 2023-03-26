eec389bdd6e3b47daf22e54d615f8517cac933fb
Caner Türe <cnrture@gmail.com>
2023-03-26 04:56:42 +0300

    color bug fix and code refactor

--- exceptionreport/src/main/java/com/canerture/exceptionreport/ExceptionReportActivity.kt
+++ exceptionreport/src/main/java/com/canerture/exceptionreport/ExceptionReportActivity.kt
@@ -9,7 +9,7 @@ import android.text.Layout
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.method.LinkMovementMethod
-import android.text.style.AlignmentSpan
+import android.text.style.AlignmentSpan.Standard
 import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
 import android.view.WindowManager
@@ -31,30 +31,30 @@ class ExceptionReportActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         val exceptionText = intent.getStringExtra("exceptionText") as String
-        val themeColor = intent.getIntExtra("themeColor", 0)
+        val themeColor = intent.getIntExtra("themeColor", R.color.black)
+
+        val color = ContextCompat.getColor(this@ExceptionReportActivity, themeColor)
 
         with(binding) {
 
-            val color = ContextCompat.getColor(this@ExceptionReportActivity, themeColor)
-
-            if (themeColor != 0) {
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-                window.statusBarColor = color
-
-                tvTitle.setTextColor(color)
-                btnClose.setStrokeColorResource(themeColor)
-                btnClose.setTextColor(color)
-                btnCopy.setBackgroundColor(color)
+            window.apply {
+                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                statusBarColor = color
             }
 
-            tvExceptionText.setClarificationText(exceptionText, color)
+            tvTitle.setTextColor(color)
+            btnClose.setStrokeColorResource(themeColor)
+            btnClose.setTextColor(color)
+            btnCopy.setBackgroundColor(color)
+
+            tvExceptionText.setExceptionText(exceptionText, color)
 
             btnCopy.setOnClickListener {
-                val clipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
-                val clipData = ClipData.newPlainText("text", tvExceptionText.text.toString())
-                clipboardManager.setPrimaryClip(clipData)
-                Snackbar.make(it, getString(R.string.text_copied), DURATION).show()
+                (getSystemService(CLIPBOARD_SERVICE) as ClipboardManager).apply {
+                    setPrimaryClip(ClipData.newPlainText("text", exceptionText))
+                    Snackbar.make(it, getString(R.string.text_copied), DURATION).show()
+                }
             }
 
             btnClose.setOnClickListener {
@@ -63,54 +63,30 @@ class ExceptionReportActivity : AppCompatActivity() {
         }
     }
 
-    private fun TextView.setClarificationText(
-        fullText: String,
-        color: Int
-    ) {
-        SpannableString(fullText).apply {
-            setCustomSpan(
-                fullText = fullText,
-                color = color,
-                Pair("Android Version:", true),
-                Pair("Device:", true),
-                Pair("Date:", true),
-                Pair("Error", false)
-            )
+    private fun TextView.setExceptionText(exceptionText: String, color: Int) {
+        SpannableString(exceptionText).apply {
+            listOf("Android Version:", "Device:", "Date:", "Error").forEach {
+                setSpan(ForegroundColorSpan(color), exceptionText, it)
+                setSpan(StyleSpan(Typeface.BOLD), exceptionText, it)
+                if (it != "Error")
+                    setSpan(Standard(Layout.Alignment.ALIGN_CENTER), exceptionText, it)
+            }
             text = this
             movementMethod = LinkMovementMethod.getInstance()
             highlightColor = Color.TRANSPARENT
         }
     }
 
-    private fun SpannableString.setCustomSpan(
-        fullText: String,
-        color: Int,
-        vararg text: Pair<String, Boolean>
-    ) {
-        text.forEach {
-            if (it.second)
-                setSpan(
-                    AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER),
-                    fullText.indexOf(it.first),
-                    fullText.indexOf(it.first) + it.first.length,
-                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-                )
-            setSpan(
-                ForegroundColorSpan(color),
-                fullText.indexOf(it.first),
-                fullText.indexOf(it.first) + it.first.length,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            setSpan(
-                StyleSpan(Typeface.BOLD),
-                fullText.indexOf(it.first),
-                fullText.indexOf(it.first) + it.first.length,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-        }
-    }
+    private fun SpannableString.setSpan(spanType: Any, fullText: String, colorText: String) =
+        setSpan(
+            spanType,
+            fullText.indexOf(colorText),
+            fullText.indexOf(colorText) + colorText.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
 
     companion object {
         private const val DURATION = 1000
     }
 }
+
