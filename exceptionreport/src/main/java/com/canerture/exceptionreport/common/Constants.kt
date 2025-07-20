efd29f4b87b49ab880299b9f0548122d3348b66a
candroid <cnrture@gmail.com>
2025-07-20 20:09:39 +0300

    Add parsed stack trace handling to ExceptionReport and update onExceptionReceived signature

--- exceptionreport/src/main/java/com/canerture/exceptionreport/common/Constants.kt
+++ exceptionreport/src/main/java/com/canerture/exceptionreport/common/Constants.kt
@@ -3,6 +3,7 @@ package com.canerture.exceptionreport.common
 object Constants {
     const val EXCEPTION_TEXT = "exceptionText"
     const val DEVICE_INFO = "deviceInfo"
+    const val PARSED_STACK_TRACE = "parsedStackTrace"
 
     const val DATE_FORMAT = "dd/MM/yyyy HH:mm:ss"
 }
--- exceptionreport/src/main/java/com/canerture/exceptionreport/ui/ExceptionReport.kt
+++ exceptionreport/src/main/java/com/canerture/exceptionreport/ui/ExceptionReport.kt
@@ -5,30 +5,31 @@ import android.content.Intent
 import android.os.Build
 import android.os.Process
 import com.canerture.exceptionreport.R
-import com.canerture.exceptionreport.common.Constants.DATE_FORMAT
 import com.canerture.exceptionreport.common.Constants.DEVICE_INFO
 import com.canerture.exceptionreport.common.Constants.EXCEPTION_TEXT
+import com.canerture.exceptionreport.common.Constants.PARSED_STACK_TRACE
+import com.canerture.exceptionreport.common.StackTraceElement
+import com.canerture.exceptionreport.common.getCurrentDate
+import com.canerture.exceptionreport.common.getPackageName
+import com.canerture.exceptionreport.common.parseStackTrace
 import java.io.PrintWriter
 import java.io.StringWriter
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 import kotlin.system.exitProcess
 
 class ExceptionReport(private val activity: Activity) : Thread.UncaughtExceptionHandler {
 
-    private var onExceptionReceived: (String, String) -> Unit = { _, _ -> }
+    private var onExceptionReceived: (String, String, ArrayList<StackTraceElement>) -> Unit = { _, _, _ -> }
 
     private var targetActivity: Class<*>? = null
 
     init {
         Thread.setDefaultUncaughtExceptionHandler(this)
 
-        onExceptionReceived = { deviceInfo, exceptionText ->
+        onExceptionReceived = { deviceInfo, exceptionText, parsedStackTrace ->
             Intent(activity, targetActivity ?: ExceptionReportActivity::class.java).apply {
                 putExtra(DEVICE_INFO, deviceInfo)
                 putExtra(EXCEPTION_TEXT, exceptionText)
+                putExtra(PARSED_STACK_TRACE, parsedStackTrace)
                 activity.startActivity(this)
             }
             Process.killProcess(Process.myPid())
@@ -39,21 +40,19 @@ class ExceptionReport(private val activity: Activity) : Thread.UncaughtException
     override fun uncaughtException(thread: Thread, exception: Throwable) {
         val stackTrace = StringWriter()
         exception.printStackTrace(PrintWriter(stackTrace))
+        val stackTraceString = stackTrace.toString()
+
+        val packageName = stackTraceString.getPackageName()
+        val parsedStackTrace = ArrayList(stackTraceString.parseStackTrace(packageName))
 
         with(activity) {
             StringBuilder().apply {
                 append("${getString(R.string.android_version)} ${Build.VERSION.RELEASE}\n")
                 append("${Build.BRAND.uppercase()} - ${Build.DEVICE.uppercase()}\n")
-                append(getDate())
+                append(getCurrentDate())
 
-                onExceptionReceived(this.toString(), stackTrace.toString())
+                onExceptionReceived(this.toString(), stackTraceString, parsedStackTrace)
             }
         }
-    }
-
-    private fun getDate(): String {
-        val dateFormat: DateFormat = SimpleDateFormat(DATE_FORMAT, Locale.getDefault())
-        val date = Date()
-        return dateFormat.format(date).toString()
     }
 }
